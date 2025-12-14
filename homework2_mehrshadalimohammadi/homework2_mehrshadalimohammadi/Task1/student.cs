bded5a7c67e5f8b1672bd9e71e2141686515e59d
Mehrshad <mehrshad.am.04@gmail.com>
2025-12-14 22:45:27 +0330

    implementation of homework2

---
+++ homework2_mehrshadalimohammadi/homework2_mehrshadalimohammadi/Task1/student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework2_mehrshadalimohammadi.Task1
+{
+    public class student
+    {
+        public student(int age, string name)
+        {
+            this.Age = age;
+            this.Name = name;
+        }
+
+
+        public int Age { get; set; }
+
+        public string Name { get; set; }
+
+        public void Introduce()
+        {
+            Console.WriteLine($"Hello , my name is {Name} and I am {Age} years old");
+        }
+    }
+}
---
+++ homework2_mehrshadalimohammadi/homework2_mehrshadalimohammadi/Task3/Statistics.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework2_mehrshadalimohammadi.Task3
+{
+    public class Statistics
+    {
+        public void AnalyzeNumbers(int num1, ref int num2, out int num3)
+        {
+            num3 = num1 + num2;
+            num2 = num2 * num2;
+            Console.WriteLine(num1);
+        }
+    }
+}
